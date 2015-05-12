38bbe8e8fc6a030032308481a2c7e25bdd44bb6d
mangledjambon <MotorHead666>
2015-05-12 15:36:09 +0100

    added code for random generation of floats using different bounds

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <stdlib.h>
 
 void print(float f);
 void add(float* f);
@@ -7,17 +8,30 @@ void add(float* f);
 using namespace std;
 
 int main() {
-
+/*
     // shows the inaccuracy of floating point
     // addition with small numbers
     float lf = 4.0000000000000000000f;
-
     for (int i = 0; i < 1000; i++)
     {
         cout << i << ". ";
         add(&lf);
         print(lf);
-    }
+    }*/
+
+    // random float generation
+    //This will generate a number from 0.0 to 1.0, inclusive.
+    float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+    //This will generate a number from 0.0 to some arbitrary float, X:
+    float X = 10.f;
+    float r2 = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/X));
+
+    //This will generate a number from some arbitrary LO to some arbitrary HI:
+    float LO = 0.f;
+    float HI = 4.f;
+    float r3 = LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO)));
+
 
     return 0;
 }
@@ -29,7 +43,7 @@ void print(float f)
 
 void add(float* f)
 {
-    // doesn't add anything when printed
+    // doesn't add anything
     //*f += 0.0000001f;
 
     // adds
